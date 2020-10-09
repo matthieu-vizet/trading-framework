a239ef96083a0fbf041b241086a0b1eefc525a21
matthieu-secp256k1 <52050739+matthieu-secp256k1@users.noreply.github.com>
2020-10-09 20:07:36 +0200

    Add files via upload

---
+++ ammswap.py
@@ -0,0 +1,12 @@
+# quantity to swap expressed in x
+def getswap(x,y,quantity):
+    k = x * y
+    nx = x + quantity
+    ny = k / nx
+    pricey = y - ny
+    pricex = quantity / pricey
+    slippage = (pricex - (x/y)) / (x/y)
+    return pricex, slippage
+
+    
+    
